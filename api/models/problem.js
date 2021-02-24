3f8ed5e98585d6251628c80962284e9ca6cea265
Pranay Piyush <pranay.piyush2508@gmail.com>
2021-02-24 23:12:38 +0530

    Added express mongo sanitize

---
+++ api/models/problem.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const problemSchema = mongoose.Schema(
+    {
+        _id: mongoose.Schema.Types.ObjectId,
+        problemTitle: String,
+        problemCategory: String,
+        problemDesc: String,
+        studentEmail: String,
+        studentName: String,
+        studentId: String,
+        studentRoomNo: String,
+        studentBhawan: String
+    }
+);
+
+
+module.exports = mongoose.model("Problem",problemSchema);
