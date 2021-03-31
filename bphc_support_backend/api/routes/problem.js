c5b909c24b5616434e6fa72e4e7abb31b43ac5b0
KoderKaul <f20180325@hyderbad.bits-pilani.ac.in>
2021-04-01 01:34:44 +0530

    adding bin for heroku

--- bphc_support_backend/api/routes/problem.js
+++ bphc_support_backend/api/routes/problem.js
@@ -1,81 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const checkAuth = require('../middleware/check-auth');
-const sanitizer = require('sanitize')();
-const { check } = require('express-validator');
-
+const mongoose = require("mongoose");
+const checkAuth = require("../middleware/check-auth");
+const sanitizer = require("sanitize")();
+const { check } = require("express-validator");
 
 const Problem = require("../models/problem");
 
-router.post('/', checkAuth,(req, res, next) => {
+router.post("/", checkAuth, (req, res, next) => {
+  const problem = new Problem();
 
-    const problem = new Problem();
+  problem["_id"] = new mongoose.Types.ObjectId();
+  problem["problemDate"] = new Date();
 
-    problem["_id"]=new mongoose.Types.ObjectId();
-    problem["problemDate"]=new Date();
+  for (var attr in req.body) {
+    problem[attr] = req.body[attr];
+  }
 
-    for(var attr in req.body)
-    {
-      problem[attr]=req.body[attr];
-    }
-
-    problem.save()
-    .then(result =>{
-        console.log(result);
+  problem
+    .save()
+    .then((result) => {
+      console.log(result);
     })
-    .catch(err => {
-        console.log(err);
-    });
-
-    res.status(201).json({
-        message: "handling post requests",
-        createdproblemt: problem
-    });
-    
-    
-
-
-});
-
-
-router.get('/user', [check('email').isEmail().normalizeEmail()], checkAuth, (req, res,next) => {
-  
-  const email = sanitizer.value(req.body.email,String);
-
-  Problem.find({studentEmail: email})
-  .exec()
-  .then(doc =>{
-    res.status(200).json(doc);
-  }
-  )
-  .catch(err => {
-    console.log(err);
-  }
-  );
-
-});
-
-router.get('/admin', checkAuth, (req, res,next) => {
-  
-  const bhawan = sanitizer.value(req.body.bhawan,String);
-  
-    Problem.find({bhawan: bhawan})
-    .exec()
-    .then(doc =>{
-      res.status(200).json(doc);
-    }
-    )
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-    }
-    );
-  
+    });
+
+  res.status(201).json({
+    message: "handling post requests",
+    createdproblemt: problem,
   });
+});
+router.get("/", (req, res, next) => {
+  Problem.find()
+    .exec()
+    .then((doc) => {
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
+router.get(
+  "/user",
+  [check("email").isEmail().normalizeEmail()],
+  checkAuth,
+  (req, res, next) => {
+    const email = sanitizer.value(req.body.email, String);
 
+    Problem.find({ studentEmail: email })
+      .exec()
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+);
 
+router.get("/admin", checkAuth, (req, res, next) => {
+  const bhawan = sanitizer.value(req.body.bhawan, String);
 
-
+  Problem.find({ bhawan: bhawan })
+    .exec()
+    .then((doc) => {
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
