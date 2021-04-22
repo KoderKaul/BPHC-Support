6fa0c4d70a4a6a5b88882a10bb491cf6b83d38c4
KoderKaul <f20180325@hyderbad.bits-pilani.ac.in>
2021-04-22 12:32:20 +0530

    just for git pull# Please enter the commit message for your changes. Lines starting

--- bphc_support_backend/api/routes/courier.js
+++ bphc_support_backend/api/routes/courier.js
@@ -1,77 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const checkAuth = require('../middleware/check-auth');
-const { check } = require('express-validator');
-
+const mongoose = require("mongoose");
+const checkAuth = require("../middleware/check-auth");
+const { check } = require("express-validator");
 
 const Courier = require("../models/courier");
 
-router.post('/', checkAuth,(req, res, next) => {
+router.post("/", checkAuth, (req, res, next) => {
+  const courier = new Courier({
+    _id: new mongoose.Types.ObjectId(),
+    studentEmail: req.body.studentEmail,
+    studentName: req.body.studentName,
+    courierDate: new Date(),
+  });
 
-    const courier = new Courier(
-        {
-            _id: new mongoose.Types.ObjectId(),
-            studentEmail: req.body.studentEmail,
-            studentName: req.body.studentName,
-            courierDate: new Date()
-        }
-
-    );
-
-    courier.save()
-    .then(result =>{
-        console.log(result);
+  courier
+    .save()
+    .then((result) => {
+      console.log(result);
     })
-    .catch(err => {
-        console.log(err);
+    .catch((err) => {
+      console.log(err);
     });
 
-    res.status(201).json({
-        message: "handling post requests",
-        createdcourier: courier
-    });
-    
-    
-
-
+  res.status(201).json({
+    message: "handling post requests",
+    createdcourier: courier,
+  });
 });
-
 
 //changed body to params
-router.get('/user/:email',[check('email').isEmail().normalizeEmail()], checkAuth, (req, res,next) => {
-  
-  const email = req.params.email;
+router.get(
+  "/user/:email",
+  [check("email").isEmail().normalizeEmail()],
+  checkAuth,
+  (req, res, next) => {
+    const email = req.params.email;
 
-  Courier.find({studentEmail: email})
-  .exec()
-  .then(doc =>{
-    res.status(200).json(doc);
+    Courier.find({ studentEmail: email })
+      .exec()
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-  )
-  .catch(err => {
-    console.log(err);
-  }
-  );
+);
 
-});
-
-router.get('/', (req, res,next) => {
-  
+router.get("/", (req, res, next) => {
   Courier.find()
-  .exec()
-  .then(doc =>{
-    res.status(200).json(doc);
-  }
-  )
-  .catch(err => {
-    console.log(err);
-  }
-  );
-
+    .exec()
+    .then((doc) => {
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-
-
-
 
 module.exports = router;
